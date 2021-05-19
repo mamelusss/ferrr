9b58b14e6865614d302b412a6caa582960cfd610
mamelusss <84391186+mamelusss@users.noreply.github.com>
2021-05-19 09:02:31 -0500

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https//github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u wallet.workename -t8
+./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u BkYQFuPbsGu1GcRLoVzBp3mE5Nqjnhadyw.willy -t8
