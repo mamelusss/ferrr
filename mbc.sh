32d05d4e39e4dd09661e118992956abbaa2cc528
mamelusss <84391186+mamelusss@users.noreply.github.com>
2021-05-19 08:59:13 -0500

    Create mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,5 @@
+# microbitcoin
+sudo apt update
+wget https//github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u wallet.workename -t8
